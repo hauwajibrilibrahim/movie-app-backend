54022d7348a0f6ae63c7cab78baf7e132810eb92
hauwajibrilibrahim <hauwajibrilibrahim@gmail.com>
2025-07-26 17:24:11 +0100

    configure CORS middleware to allow requests from the frontend

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['https://movieemuse.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: false 
+}));
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/recommendations', recommendationRoutes);
